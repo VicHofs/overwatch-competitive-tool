997d9ddff7fe08b7b4f2268d9008e7a92ade2744
Victor Hofstetter <victorbarroshof@gmail.com>
2023-06-26 17:32:29 -0500

    Updates

--- src/pages/Tools/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 30px;
-  min-height: calc(100vh - 160px);
-`;
--- src/services/owapi/index.tsx
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const owapi = axios.create({ baseURL: 'https://owapi.io/profile/pc/us' });
-
-export default owapi;
